6b7429ad73033258e7a0e3304e0c82bd9aef0e20
Soulittude <greatmaotun@gmail.com>
2022-06-23 15:29:08 +0300

    first commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float forwardSpeed;
+    [SerializeField]
+    private float sideSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        forwardSpeed = 5;
+        sideSpeed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal") * sideSpeed * Time.deltaTime;
+        this.transform.Translate(xAxis, 0, forwardSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ToplayiciKup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToplayiciKup : MonoBehaviour
+{
+    GameObject coreCube;
+    int yukseklik;
+    
+    private GameManager gm;
+
+    void Start()
+    {
+        coreCube = GameObject.Find("CoreCube");
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    void Update()
+    {
+        coreCube.transform.position = new Vector3(transform.position.x, yukseklik + 1, transform.position.z);
+        this.transform.localPosition = new Vector3(0, -yukseklik, 0);
+    }
+
+    public void YukseklikAzalt()
+    {
+        yukseklik--;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Stack" && other.gameObject.GetComponent<StackCube>().GetToplandiMi() == false)
+        {
+            yukseklik += 1;
+            
+            other.gameObject.GetComponent<StackCube>().ToplandiYap();
+            other.gameObject.GetComponent<StackCube>().KatAyarla(yukseklik);
+
+            other.gameObject.transform.parent = coreCube.transform;
+        }
+
+        else if(other.gameObject.tag == "Void")
+        {
+            gm.restartGame();
+        }
+
+    }
+}
